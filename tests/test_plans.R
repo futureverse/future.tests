6dca2ff170f544d307c6dcac987406b076678b7e
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-03-15 09:35:22 -0700

    CLEANUP: Don't test future::cluster, because it will be removed

--- tests/test_plans.R
+++ tests/test_plans.R
@@ -4,14 +4,13 @@ library(future)
 test_plans <- test_plans()
 print(test_plans)
 
-add_test_plan(plan(future:::constant))
 add_test_plan(plan(sequential))
 add_test_plan(plan(multisession, workers = 1L))
 add_test_plan(plan(multisession, workers = 2L))
 
 test_plans <- test_plans()
 print(test_plans)
-stopifnot(length(test_plans) == 4L)
+stopifnot(length(test_plans) == 3L)
 
 res <- along_test_plans({ 42L })
 print(res)
